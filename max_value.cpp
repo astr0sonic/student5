617a56de760fdcf1a5a429f43aa4af92730dcf77
Artem <artembikulov98@gmail.com>
2024-04-01 20:34:19 +0300

    task1

--- max_value.cpp
+++ max_value.cpp
@@ -7,18 +7,18 @@ double calculateMaxValue(double* nums, int n) {
         if (nums[i] < 0)
         {
             double maxVv = maxV;
-            maxV = max({ maxV - nums[i], minV * nums[i], minV / nums[i] });
-            minV = min({ minV + nums[i], maxVv * nums[i], maxVv / nums[i] });
+            maxV = std::max({ maxV - nums[i], minV * nums[i], minV / nums[i] });
+            minV = std::min({ minV + nums[i], maxVv * nums[i], maxVv / nums[i] });
         }
         else if (nums[i] > 0)
         {
-            maxV = max({ maxV + nums[i], maxV * nums[i], maxV / nums[i] });
-            minV = min({ minV - nums[i], minV * nums[i], minV / nums[i] });
+            maxV = std::max({ maxV + nums[i], maxV * nums[i], maxV / nums[i] });
+            minV = std::min({ minV - nums[i], minV * nums[i], minV / nums[i] });
         }
         else
         {
-            minV = min(0.0, minV);
-            maxV = max(0.0, maxV);
+            minV = std::min(0.0, minV);
+            maxV = std::max(0.0, maxV);
         }
     }
     return maxV;
